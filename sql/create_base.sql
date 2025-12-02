081467903472d50d43c8523376218427f4d2f10a
Darcksid3 <109866611+Darcksid3@users.noreply.github.com>
2025-12-02 14:30:57 +0100

    Débug create_base_sql et structure site

--- sql/create_base.sql
+++ sql/create_base.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `covoiturage_Interne`;
 
 -- Sélectionne la base de données pour les opérations suivantes
-USE `Covoiturage_Interne`;
+USE `covoiturage_Interne`;
 
 CREATE TABLE IF NOT EXISTS `ville` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur_enregistre` (
     
     -- Clé étrangère vers la table `utilisateurs` (on delete cascade)
     FOREIGN KEY (`utilisateur_id`)
-        REFERENCES `utilisateurs` (`id`)
+        REFERENCES `utilisateur` (`id`)
         ON DELETE CASCADE -- Si l'utilisateur de base est supprimé, cette entrée est aussi supprimée
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,13 +40,13 @@ CREATE TABLE IF NOT EXISTS `trajet` (
     `depart_ville_id` INT UNSIGNED NOT NULL,
     
     `depart_date` DATE NOT NULL,
-    `depart_heure` heure NOT NULL,
+    `depart_heure` TIME NOT NULL,
     
     -- Clé étrangère vers la table `ville` pour la ville d'arrivée
     `arrive_ville_id` INT UNSIGNED NOT NULL,
     
     `arrive_date` DATE NOT NULL,
-    `arrive_heure` heure NOT NULL,
+    `arrive_heure` TIME NOT NULL,
     
     -- Clé étrangère vers la table `utilisateurs` (le créateur du trajet)
     `createur_id` INT UNSIGNED NOT NULL, 
@@ -65,6 +65,6 @@ CREATE TABLE IF NOT EXISTS `trajet` (
         
     -- Contrainte pour le créateur du trajet
     FOREIGN KEY (`createur_id`)
-        REFERENCES `utilisateurs` (`id`)
+        REFERENCES `utilisateur` (`id`)
         ON UPDATE CASCADE ON DELETE RESTRICT -- RESTRICT empêche la suppression d'un utilisateur tant qu'il est créateur d'un trajet
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
